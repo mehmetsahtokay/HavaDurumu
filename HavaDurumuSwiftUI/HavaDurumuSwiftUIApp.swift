f322649e76581e3a70591a9f07fdb668029b7d2b
mehmetsahtokay <mehmetsahtokay@outlook.com>
2023-04-16 02:30:39 +0300

    Initial Commit

---
+++ HavaDurumuSwiftUI/HavaDurumuSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HavaDurumuSwiftUIApp.swift
+//  HavaDurumuSwiftUI
+//
+//  Created by sah on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct HavaDurumuSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
